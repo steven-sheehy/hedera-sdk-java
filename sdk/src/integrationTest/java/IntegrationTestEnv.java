cc434aeddce182a77cf0ece6d2d0d015f3dbfb27
Daniel Akhterov <akhterovd@gmail.com>
2021-05-21 15:15:09 -0700

    wip: tls
    Signed-off-by: Daniel Akhterov <akhterovd@gmail.com>

--- sdk/src/integrationTest/java/IntegrationTestEnv.java
+++ sdk/src/integrationTest/java/IntegrationTestEnv.java
@@ -1,4 +1,9 @@
 import com.hedera.hashgraph.sdk.*;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NegotiationType;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SupportedCipherSuiteFilter;
 
 import java.util.*;
 import java.util.concurrent.TimeoutException;
@@ -46,5 +51,17 @@ public class IntegrationTestEnv {
         operatorKey = key;
         nodeAccountIds = Collections.singletonList(response.nodeId);
         client.setOperator(operatorId, operatorKey);
+
+        SslContextBuilder contextBuilder = GrpcSslContexts.configure(SslContextBuilder.forClient());
+        contextBuilder
+            .protocols(protocols)
+            .ciphers(ciphers, SupportedCipherSuiteFilter.INSTANCE);
+
+        channel = NettyChannelBuilder
+            .forAddress("35.237.182.66", 50212)
+            .negotiationType(NegotiationType.TLS)
+            .sslContext(contextBuilder.build())
+            .overrideAuthority("127.0.0.1")
+            .build();
     }
 }
--- sdk/src/main/java/com/hedera/hashgraph/sdk/ManagedNode.java
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/ManagedNode.java
@@ -1,8 +1,12 @@
 package com.hedera.hashgraph.sdk;
 
+import io.grpc.LoadBalancerRegistry;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -27,6 +31,33 @@ abstract class ManagedNode {
         if (channel != null) {
             return channel;
         }
+
+        var registry = LoadBalancerRegistry.getDefaultRegistry();
+        var protocols = new String[] {"TLSv1.2", "TLSv1.3"};
+        var ciphers = Collections.singletonList("TLS_DHE_RSA_WITH_AES_256_GCM_SHA384");
+
+        try {
+            var NettyChannelBuilder = Class.forName("NettyChannelBuilder");
+            var SslContextBuilder = Class.forName("SslContextBuilder");
+            var GrpcSslContexts = Class.forName("GrpcSslContexts");
+
+            var NettyChannelBuilder_forAddress = NettyChannelBuilder.getMethod("forAddress");
+            NettyChannelBuilder_forAddress.invoke(address);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+        SslContextBuilder contextBuilder = GrpcSslContexts.configure(SslContextBuilder.forClient());
+        contextBuilder
+            .protocols(protocols)
+            .ciphers(ciphers, SupportedCipherSuiteFilter.INSTANCE);
+
+        channel = NettyChannelBuilder
+            .forAddress("35.237.182.66", 50212)
+            .negotiationType(NegotiationType.TLS)
+            .sslContext(contextBuilder.build())
+            .overrideAuthority("127.0.0.1")
+            .build();
 
         channel = ManagedChannelBuilder.forTarget(address)
             .usePlaintext()
